319e17397af4c59bbe0c1443a1b8aaa074211c68
Laura <laura.barona@uao.edu.co>
2026-06-22 22:07:24 -0500

    Deleted project config vite

--- src/components/footer/footer.js
+++
@@ -1,9 +0,0 @@
-import "./footer.css";
-import footerTemplate from "./footer.html?raw";
-
-export function createFooter() {
-    const footer = document.createElement("div");
-    footer.innerHTML = footerTemplate;
-
-    return footer.firstElementChild;
-}
